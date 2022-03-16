73f698dbdcc39f477ca9450928e16996586b46b8
Bricktech <bricktech2000@gmail.com>
2022-03-16 00:48:02 -0400

    modified password algorithm

--- run.py
+++ run.py
@@ -1,47 +1,51 @@
-from hashlib import sha256
+from hashlib import sha256, sha1
+from functools import reduce
 import math
 import getpass
 import pyperclip
 
-#https://stackoverflow.com/questions/36490354/working-with-bytes-in-python-using-sha-256
-#input: an ascii string
-#output: a byte array containing the sha256 hash of the input
-def hash(ascii):
-    return sha256(ascii.encode('utf-8')).digest()
+# https://stackoverflow.com/questions/36490354/working-with-bytes-in-python-using-sha-256
 
-#https://stackoverflow.com/questions/44571093/how-can-i-encode-a-32bit-integer-into-a-byte-array/44571179#44571179
-#https://stackoverflow.com/questions/51181193/convert-a-8bit-list-to-a-32-bit-integer-array-in-python/51181311
-#https://stackoverflow.com/questions/25259947/convert-variable-sized-byte-array-to-a-integer-long
-#z85 table:
-#https://rfc.zeromq.org/spec/32/
-#https://github.com/zeromq/rfc/blob/master/src/spec_32.c
-#input: an array of bytes
-#output: a string containing the Z85 representation of the input
+# https://stackoverflow.com/questions/44571093/how-can-i-encode-a-32bit-integer-into-a-byte-array/44571179#44571179
+# https://stackoverflow.com/questions/51181193/convert-a-8bit-list-to-a-32-bit-integer-array-in-python/51181311
+# https://stackoverflow.com/questions/25259947/convert-variable-sized-byte-array-to-a-integer-long
+# z85 table:
+# https://rfc.zeromq.org/spec/32/
+# https://github.com/zeromq/rfc/blob/master/src/spec_32.c
+# input: an array of bytes
+# output: a string containing the Z85 representation of the input
 def encode(bytes):
-    out = ''
-    table = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.-:+=^!/*?&<>()[]{}@%$#'
-    value = 0
-    for i in range(len(bytes)):
-        #make 32-bit integer
-        value = value * 256 + bytes[i]
-        if i % 4 == 3:
-            #convert to printable characters using Z85
-            divisor = int(math.pow(85, 4))
-            while divisor:
-                out += table[value // divisor % 85]
-                divisor //= 85
-            value = 0
-    return out
+  out = ''
+  table = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.-:+=^!/*?&<>()[]{}@%$#'
+  value = 0
+  for i in range(len(bytes)):
+    # make 32-bit integer
+    value = value * 256 + bytes[i]
+    if i % 4 == 3:
+      # convert to printable characters using Z85
+      divisor = int(math.pow(85, 4))
+      while divisor:
+        out += table[value // divisor % 85]
+        divisor //= 85
+      value = 0
+  return out
 
-def concat(name, user, master):
-    return '%'.join((name, user, master))
+# https://stackoverflow.com/questions/29408173/byte-operations-xor-in-python
+# input: a list of ascii strings to be joined (XORed)
+# output: a byte array containing the joined result
+def join(*args):
+  def xor(bytes1, bytes2):
+    return bytes(a ^ b for a, b in zip(bytes1, bytes2))
+
+  return reduce(xor, [sha1(arg.encode('utf-8')).digest() for arg in args])
 
 
-print('DBLess password manager\n')
-name = input('domain name: ').lower()
-user = input('username: ').lower()
-master = getpass.getpass('master password: ')
-password = encode(hash(concat(name, user, master)))
-#https://stackoverflow.com/questions/11063458/python-script-to-copy-text-to-clipboard
+print('DBLess Password Manager\n')
+name = input('Service: ').lower()
+user = input('Username: ').lower()
+master = getpass.getpass('Master Password: ')
+password = encode(sha256(join(name, user, master)).digest())
+
+# https://stackoverflow.com/questions/11063458/python-script-to-copy-text-to-clipboard
 pyperclip.copy(password)
-print('password has been copied to clipboard')
+print('\nPassword has been copied to clipboard.')
